d02f191041bb102c6b909a6eeadfb73e5ba0bbdd
darkheart101 <kurokokoro@windowslive.com>
2020-04-01 06:47:28 +0300

    feature: password encoder

--- springblog/src/main/java/com/tkouleris/springblog/service/AuthService.java
+++ springblog/src/main/java/com/tkouleris/springblog/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.tkouleris.springblog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.tkouleris.springblog.dto.RegisterRequest;
@@ -12,14 +13,21 @@ public class AuthService {
 	
 	@Autowired
 	private UserRepository R_User;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	public void signUp(RegisterRequest registerRequest)
 	{
 		User user = new User();
 		user.setUserName(registerRequest.getUsername());
-		user.setPassword(registerRequest.getPassword());
+		user.setPassword(encodePassword(registerRequest.getPassword()));
 		user.setEmail(registerRequest.getEmail());
 
 		R_User.save(user);
+	}
+	
+	private String encodePassword(String password)
+	{
+		return passwordEncoder.encode(password);
 	}
 }
